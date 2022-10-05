0fd2bd58d85a300fa6ac9cfaec396e15a80c4e57
Danyl Miniailo <danylo.miniailo@care.com>
2022-10-05 20:18:15 +0300

    Add largest mars picture feature

---
+++ src/test/java/com/changer/marspictures/MarsPicturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changer.marspictures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsPicturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
